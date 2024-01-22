23a9d702552cdaa60e8e9343bb77a356fb30b5a7
hissan.shah@motsd.org <programming@mort11.org>
2024-01-22 18:31:59 -0500

    Changed names in Vision subsystem

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -3,7 +3,9 @@ import frc.robot.Util.Constants.Vision.Pipeline;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.DriverStation; 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.hal.AllianceStationID;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 
@@ -18,107 +20,183 @@ public class Vision extends SubsystemBase {
         llTable = NetworkTableInstance.getDefault().getTable("limelight");
     }
 
+	/**
+	 * 
+	 * @return
+	 */
     public boolean hasTarget() {
 		return llTable.getEntry("tv").getInteger(0) == 1;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
     public double getTargetArea() {
 		return llTable.getEntry("ta").getDouble(0);
 	}
 
+	/**
+	 * 
+	 * @param id
+	 */
 	public void setPipeline(int id) {
 		llTable.getEntry("pipeline").setNumber(id);
 	}
 
+	/**
+	 * 
+	 * @param pipe
+	 */
 	public void setPipeline(Pipeline pipe) {
 		setPipeline(pipe.getId());
 	}
 
-	public void setPipeline(Pipeline pipe, int ATID) {
-		setPipeline(pipe.getId(ATID));
+	/**
+	 * 
+	 * @param pipe
+	 * @param AprilTagID
+	 */
+	public void setPipeline(Pipeline pipe, int AprilTagID) {
+		setPipeline(pipe.getId(AprilTagID));
 	}
 
-	public Pipeline getPipeline() {
+	/**
+	 * 
+	 * @return
+	 */
+	public Pipeline getPipelineID() {
 		return Pipeline.getPipeline((int) llTable.getEntry("pipeline").getInteger(0));
 	}
 
 	// Translation (x, y, z) Rotation(pitch, yaw, roll)
-	public Number[] getCamTran() {
+	/**
+	 * 
+	 * @return
+	 */
+	public Number[] getCamTranslation() {
 		return llTable.getEntry("targetpose_robotspace").getNumberArray(new Number[0]);
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public double getX() {
 		return llTable.getEntry("tx").getDouble(0);
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public double getY() {
 		return llTable.getEntry("ty").getDouble(0);
 	}
 
-	public double getCamTranX() {
-		if (getCamTran().length < 1) {
+	/**
+	 * 
+	 * @return
+	 */
+	public double getCamTranslationX() {
+		if (getCamTranslation().length < 1) {
 			return 0;
 		}
 
-		return (double) getCamTran()[0];
+		return (double) getCamTranslation()[0];
 	}
 
-	public double getCamTranY() {
-		if (getCamTran().length < 1) {
+	/**
+	 * 
+	 * @return
+	 */
+	public double getCamTranslationY() {
+		if (getCamTranslation().length < 1) {
 			return 0;
 		}
-		return (double) getCamTran()[1];
+		return (double) getCamTranslation()[1];
 	}
 
-	public double getCamTranZ() {
-		if (getCamTran().length < 1) {
+	/**
+	 * 
+	 * @return
+	 */
+	public double getCamTranslationZ() {
+		if (getCamTranslation().length < 1) {
 			return 0;
 		}
-		return (double) getCamTran()[2];
+		return (double) getCamTranslation()[2];
 	}
 
-	public double getCamTranPitch() {
-		if (getCamTran().length < 1) {
+	/**
+	 * 
+	 * @return
+	 */
+	public double getCamTranslationPitch() {
+		if (getCamTranslation().length < 1) {
 			return 0;
 		}
-		return (double) getCamTran()[3];
+		return (double) getCamTranslation()[3];
 	}
 
-	public double getCamTranYaw() {
-		if (getCamTran().length < 1) {
+	/**
+	 * 
+	 * @return
+	 */
+	public double getCamTranslationYaw() {
+		if (getCamTranslation().length < 1) {
 			return 0;
 		}
-		return (double) getCamTran()[4];
+		return (double) getCamTranslation()[4];
 	}
 
-	public double getCamTranRoll() {
-		if (getCamTran().length < 1) {
+	/**
+	 * 
+	 * @return
+	 */
+	public double getCamTranslationRoll() {
+		if (getCamTranslation().length < 1) {
 			return 0;
 		}
-		return (double) getCamTran()[5];
+		return (double) getCamTranslation()[5];
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public Pose2d getPose() {
 		double[] poseNum = new double[6];
 
-		if (DriverStation.getAlliance() == DriverStation.Alliance.Red) {
-			poseNum = llTable.getEntry("botpose_wpired").getDoubleArray(new double[6]);
-		} else {
-			poseNum = llTable.getEntry("botpose_wpiblue").getDoubleArray(new double[6]);
-		}
+		// if (DriverStation.getAlliance() == DriverStation.Alliance.Red) {
+		// 	poseNum = llTable.getEntry("botpose_wpired").getDoubleArray(new double[6]);
+		// } else {
+		// 	poseNum = llTable.getEntry("botpose_wpiblue").getDoubleArray(new double[6]);
+		// }
 
 		return new Pose2d(poseNum[0], poseNum[1], new Rotation2d(Math.toRadians(poseNum[5])));
 
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public double getLatency() {
 		return llTable.getEntry("botpose").getDoubleArray(new double[6])[6];
 	}
 
-	public int getATId() {
+	/**
+	 * 
+	 * @return
+	 */
+	public int getAprilTagId() {
 		return (int) llTable.getEntry("tid").getInteger(0);
 	}
 
+	/**
+	 * 
+	 */
 	public static Vision getInstance() {
 		if (vision == null) {
 			vision = new Vision();
