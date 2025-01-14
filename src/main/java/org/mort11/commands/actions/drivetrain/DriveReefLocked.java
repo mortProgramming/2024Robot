d2b6a6741ae1e5c420f40818e928fb788790f318
Programming 1 <programming@mort11.org>
2025-01-13 20:55:57 -0500

    made constants class filled out

--- src/main/java/org/mort11/commands/actions/drivetrain/DriveReefLocked.java
+++ src/main/java/org/mort11/commands/actions/drivetrain/DriveReefLocked.java
@@ -1,5 +1,47 @@
 package org.mort11.commands.actions.drivetrain;
 
-public class DriveReefLocked {
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.Command;
+
+import java.util.function.DoubleSupplier;
+
+import org.mort11.subsystems.Drivetrain;
+
+public class DriveReefLocked extends Command {
+    private Drivetrain drivetrain;
+
+	private DoubleSupplier translationXSupplier;
+	private DoubleSupplier translationYSupplier;
     
+    public DriveReefLocked(DoubleSupplier translationXSupplier, DoubleSupplier translationYSupplier) {
+        drivetrain = Drivetrain.getInstance();
+
+		this.translationXSupplier = translationXSupplier;
+		this.translationYSupplier = translationYSupplier;
+		
+		addRequirements(drivetrain);
+    }
+
+    @Override
+	public void execute() {
+		drivetrain.setDrive(
+			ChassisSpeeds.fromFieldRelativeSpeeds(
+			translationXSupplier.getAsDouble(),
+			translationYSupplier.getAsDouble(), 
+			0,
+			drivetrain.getIMURotation())
+        );
+
+        drivetrain.setAngleController(0);
+	}
+
+    @Override
+	public boolean isFinished() {
+		return false;
+	}
+
+    @Override
+	public void end(boolean interrupted) {
+		drivetrain.setDrive(new ChassisSpeeds(0.0, 0.0, 0.0));
+	}
 }
--- src/main/java/org/mort11/config/constants/FieldConstants.java
+++ src/main/java/org/mort11/config/constants/FieldConstants.java
@@ -7,12 +7,47 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation3d;
 
-public class FieldConstants {
-    public static final double FIELD_LENGTH = 3; //16.541
-    public static final double FIELD_WIDTH = 3; //8.211
+public final class FieldConstants {
+    public final class Reef {
+        public static final double FIELD_LENGTH = 3; //16.541
+        public static final double FIELD_WIDTH = 3; //8.211
 
-    public static final double REEF_CORNER_30_DEG_X = 0;
-    public static final double REEF_CORNER_30_DEG_Y = 0;
+        public static final double REEF_CORNER_30_DEG_X = 1;
+        public static final double REEF_CORNER_30_DEG_Y = 2;
+
+        public static final double REEF_CORNER_90_DEG_X = 1;
+        public static final double REEF_CORNER_90_DEG_Y = 2;
+
+        public static final double REEF_CORNER_150_DEG_X = 1;
+        public static final double REEF_CORNER_150_DEG_Y = 2;
+
+        public static final double REEF_CORNER_210_DEG_X = 1;
+        public static final double REEF_CORNER_210_DEG_Y = 2;
+
+        public static final double REEF_CORNER_270_DEG_X = 1;
+        public static final double REEF_CORNER_270_DEG_Y = 2;
+
+        public static final double REEF_CORNER_330_DEG_X = 1;
+        public static final double REEF_CORNER_330_DEG_Y = 2;
+
+        public static final double REEF_LINE_POSITIVE_SLOPE = (REEF_CORNER_30_DEG_Y - REEF_CORNER_210_DEG_Y) 
+            / (REEF_CORNER_30_DEG_X - REEF_CORNER_210_DEG_X);
+        public static final double REEF_LINE_POSITIVE_Y_INTERCEPT = REEF_CORNER_30_DEG_Y - REEF_LINE_POSITIVE_SLOPE * REEF_CORNER_30_DEG_X;
+        
+        public static final double REEF_LINE_NEGATIVE_SLOPE = (REEF_CORNER_330_DEG_Y - REEF_CORNER_150_DEG_Y) 
+            / (REEF_CORNER_330_DEG_X - REEF_CORNER_150_DEG_X);
+        public static final double REEF_LINE_NEGATIVE_Y_INTERCEPT = REEF_CORNER_330_DEG_Y - REEF_LINE_NEGATIVE_SLOPE * REEF_CORNER_330_DEG_X;
+
+        public static final boolean greaterThanVerticalReefLine(double xPos) {
+            return xPos > REEF_CORNER_90_DEG_X;
+        }
+        public static final boolean greaterThanPositiveReefLine(double xPos, double yPos) {
+            return yPos > (xPos * REEF_LINE_POSITIVE_SLOPE + REEF_LINE_POSITIVE_Y_INTERCEPT);
+        }
+        public static final boolean greaterThanNegativeReefLine(double xPos, double yPos) {
+            return yPos > (xPos * REEF_LINE_NEGATIVE_SLOPE + REEF_LINE_NEGATIVE_Y_INTERCEPT);
+        }
+    }
 
     public static final List<AprilTag> APRIL_TAGS = List.of(
         new AprilTag(
